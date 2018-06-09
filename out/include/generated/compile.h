cea1f5a4c385a22e984cf9768c7f76f21178f209
Hani <kirkirehani93@gmail.com>
2018-06-09 18:50:43 +0000

    Sdcardfs

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 9 17:51:10 UTC 2018"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 9 18:06:47 UTC 2018"
 #define LINUX_COMPILE_BY "Kirkhan93"
 #define LINUX_COMPILE_HOST "Flash"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC)"
