654f6f972d666045c61d9f131958510bd7e53f29
Hani <kirkirehani93@gmail.com>
2018-06-12 07:06:23 +0000

    Revert "Add state notifier driver"
    This reverts commit 0e67be2422baab63d6fe6a1fb43af084ebb6bcba.

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -1,8 +1,7 @@
 /*
  * State Notifier Driver
  *
-
- * Copyright (c) 2013-2017, Pranav Vashi <neobuddy89@gmail.com>
+ * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
