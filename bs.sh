01ba9fc3864bc8351b82679e1e32f32e010fa72c
Hani <kirkirehani93@gmail.com>
2018-07-01 12:48:05 +0000

    Build Script: Bump to 1.6

--- bs.sh
+++ bs.sh
@@ -24,7 +24,7 @@ ANYKERNEL_DIR=/home/kirkirehani93/AnyKernel2
 EXPORT_DIR=/home/kirkirehani93/flashablezips
 
 # Make Changes to this before release
-ZIP_NAME="Kirks-R1.5"
+ZIP_NAME="Kirks-R1.6"
 
 # Tweakable Options Below
 export ARCH=arm64
